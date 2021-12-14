975160aee027f45aadb3520ade8d330e69c7edaf
ByjusBrazil <91496228+ByjusBrazil@users.noreply.github.com>
2021-12-14 10:40:32 -0300

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,25 @@
+class Form {
+  constructor() {
+    // CRIE OS ELEMENTOS DA CLASSE
+  }
+
+  setElementPosition() {
+    // DEFINA A POSIÇÃO DOS ELEMENTOS
+  }
+
+
+  hide() {
+   // USE A FUNÇÃO PARA DESAPARECER OS ELEMENTOS
+  }
+
+  handleMousePressed() {
+   // CRIE A FUNÇÃO DE PRESSIONAMENTO DE TECLA
+
+   /* O símbolo usado para escrever a mensagem abaixo (laranja) é a crase. */
+  }
+
+  display() {
+    // CHAME A FUNÇÃO DE POSIÇÃO DOS ELEMENTOS
+    // CHAME A FUNÇÃO DE PRESSIONAMENTO DE TECLA
+  }
+}
---
+++ js/Game.js
@@ -0,0 +1,8 @@
+class Game {
+  constructor() {}
+
+  start() {
+    // INCLUA A CLASSE DO JOGO
+    player = new Player();
+  }
+}
